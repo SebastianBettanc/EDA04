e8158b73bffce07741d722351b67897fce451bad
Sebastian Bettancourt <xp_nopraim@hotmail.com>
2021-05-15 04:45:09 -0400

    commmit2

--- red_neuronal.py
+++ red_neuronal.py
@@ -2,10 +2,12 @@ import math as mt
 import random 
 
 class Neuron:
-    def __init__(self,value,layer,weights):
+    def __init__(self,value,layer,weights,weights_aux,next_layer):#
         self.value=value
         self.layer=layer
         self.weights=weights
+        self.weights_aux=weights_aux
+        self.next_layer=next_layer
 
     def activation(self):
         f=1/(1+mt.exp(-self.value))
@@ -13,7 +15,32 @@ class Neuron:
 
     def derivate(self):
         f=self.activation()*(1-self.activation())
-        return f      
+        return f
+
+    def calculaNuevoPeso(self,learningRate,valorESperado):
+
+        if self.next_layer==None:
+            delta=(self.activation()-valorESperado)*self.derivate()
+        else:
+            sumatoria=0
+            for neuron in self.next_layer:
+
+                delta_i= (neuron.activation()-valorESperado)
+
+                for weight in self.weights:
+                    sumatoria+=delta_i*weight
+            delta=sumatoria*self.derivate()
+
+        gradient=delta*self.activation()
+        aux=list()
+
+        for weight in self.weights:
+            new_weight=weight -learningRate*gradient
+            aux.append(new_weight)
+            
+        self.weights_aux=aux
+
+        return None          
 
 class Red:
 
@@ -36,17 +63,25 @@ class Red:
                 weights=list()
 
                 for neuron in self.layers[-1]:
-                    weight=random.uniform(0,100)
+                    weight=random.random()
                     weights.append(weight)
-                A=Neuron(None,self.layers[-1],weights)
+                A=Neuron(None,self.layers[-1],weights,None,None)
                 layer.append(A)
 
         else:        
             for i in range(numero_neuronas):  
-                A=Neuron(None,None,None)
+                A=Neuron(None,None,None,None,None)
                 layer.append(A)
 
         self.layers.append(layer)    
+
+        return None
+
+    def next_layers(self):
+
+        for i in range(len(self.layers)-1):
+            for neuron in self.layers[i]:
+                neuron.next_layer=self.layers[i+1]
 
         return None
 
@@ -84,8 +119,10 @@ r.AgregarCapa(5)
 r.AgregarCapa(4)
 r.AgregarCapa(5)
 r.AgregarCapa(2)
+r.next_layers()
 
 ini_output=[30,40,60,10,4]
 output= r.predict(ini_output)
 
 print (output)
+
